70d283b2ff2c3c6c7b3d87cbfedf7483388ee7e8
Gagan <gaganindoriya0@gmail.com>
2018-09-30 23:33:16 +0530

    Initial commit

---
+++ app/src/main/java/indoriya/gagan/com/newsappstage1/NewsLoader.java
@@ -0,0 +1,32 @@
+package indoriya.gagan.com.newsappstage1;
+
+import android.content.AsyncTaskLoader;
+import android.content.Context;
+
+import java.util.List;
+
+
+public class NewsLoader extends AsyncTaskLoader<List<News>> {
+
+    private static String REQUEST_URL =
+            "http://content.guardianapis.com/search?show-tags=contributor&api-key=24e9a993-cc6e-4363-8b64-960851b55260";
+
+    public NewsLoader(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onStartLoading() {
+        forceLoad();
+    }
+
+    @Override
+    public List<News> loadInBackground() {
+        if (REQUEST_URL == null) {
+            return null;
+        }
+
+        List<News> newsList = QueryUtils.fetchNewsData(REQUEST_URL);
+        return newsList;
+    }
+}
